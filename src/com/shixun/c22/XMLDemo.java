be07c4934d1366c568c527e55e1b815972594bfc
zhangning-accp <zhangning.holley@gmail.com>
2016-02-19 11:47:14 +0800

    2016-2-19 将xml转换成json结构的示例代码。但需要说明的是，代码最后的结构并非合格的json，部分结构不符合，不过可以作为深入研究的示例。需要改部分代码实现就可以转成完全合格的json结构

---
+++ src/com/shixun/c22/XMLDemo.java
@@ -0,0 +1,58 @@
+package com.shixun.c22;
+
+import org.w3c.dom.DOMImplementation;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Text;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
+/**
+ * Created by zn on 2016/2/19.
+ * 演示xml的解析
+ */
+public class XMLDemo {
+    public static void main(String [] args) {
+
+        try {
+
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            //1. 读取一个xml
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            //使用builder解析一个xml文件
+            Document doc = builder.parse("xmldemo.xml");
+            //先获取根节点
+            Element root = doc.getDocumentElement();
+            //doc.getDocumentElement()
+            //Element ele = doc.getElementById("abc")
+            NodeList nodes = doc.getElementsByTagName("orderEntry");
+            Node node = nodes.item(0);
+            NodeList childs = node.getChildNodes();
+            for(int i = 0; i < childs.getLength(); i ++) {
+                //因为Text类型不能转成Elememt的，所以该代码有潜在的异常风险
+                if(childs.item(i).getNodeType() == Node.ELEMENT_NODE) {
+                    Element elements = (Element) childs.item(i);
+                    System.out.println(elements.getAttribute("name"));
+                }
+            }
+
+
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/com/shixun/c22/XmlPractice.java
@@ -0,0 +1,96 @@
+package com.shixun.c22;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.soap.Node;
+import java.io.IOException;
+
+/**
+ * Created by zn on 2016/2/19.
+ * 上课随堂练习的示例
+ */
+public class XmlPractice {
+    /**
+     * PhoneInfo:{
+     Brand:[{
+     name:华为,
+     type:{name:u8650}
+     },
+     {
+     name:苹果,
+     type:[{name:iphone4},{name:iphon5}]
+     }
+     ]
+     }
+     */
+    public static void main(String [] args) {
+
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = null;
+            builder = factory.newDocumentBuilder();
+            Document doc = builder.parse("src/com/shixun/c22/phone.xml");
+            //获取根节点
+            Element root = doc.getDocumentElement();
+            StringBuffer buffer = new StringBuffer();
+            buffer.append(root.getTagName() + ":{");
+            //遍历根节点下的所有子节点
+            NodeList brandList = root.getElementsByTagName("Brand");
+            if(brandList.getLength() < 2) {
+                buffer.append("Brand:{");
+            } else {
+                buffer.append("Brand:[");
+            }
+            for(int i = 0; i < brandList.getLength(); i ++) {
+                if(brandList.item(i).getNodeType() == Node.ELEMENT_NODE) {
+                    //获取Barand元素对象
+                    Element barandEle = (Element) brandList.item(i);
+                    //获取barand元素的属性
+                    //buffer.append(barandEle.getTagName() + ":{");
+                    if(brandList.getLength() > 1) {
+                        buffer.append("{");
+                    }
+                    buffer.append("name:" + barandEle.getAttribute("name") + ",");
+                    //获取barand的子元素
+                    NodeList typeList = barandEle.getElementsByTagName("Type");
+                    //如果小于1表示只有一个type，大于表示有多个type，则需要用数组来表示
+                    if(typeList.getLength() < 2) {
+                        Element type = (Element) typeList.item(0);
+                        buffer.append(type.getTagName() + ":" + type.getAttribute("name") + "},");
+                    } else {
+                        buffer.append("Type:[");
+                        for(int j = 0; j < typeList.getLength(); j ++) {
+                            Element type = (Element) typeList.item(j);
+                            if(j < typeList.getLength() - 1) {
+                                buffer.append("{name:" + type.getAttribute("name") + "},");
+                            } else {
+                                buffer.append("{name:" + type.getAttribute("name") + "}");
+                            }
+                        }
+                        buffer.append("]");
+                        buffer.append("},");
+                    }
+                }
+            }
+            if(brandList.getLength() > 1) {
+                buffer.append("]");
+            }
+
+            buffer.append("}");
+            System.out.println(buffer.toString());
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
